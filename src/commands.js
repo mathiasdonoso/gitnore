7c08625b11715d86427a9f931a3b233171bf3caf
Mathías <mathiasd88@gmail.com>
2021-10-11 15:40:48 -0300

    Simplifying error handling without promises

--- src/commands.js
+++ src/commands.js
@@ -1,9 +1,6 @@
 #! /usr/bin/env node
 const yargs = require("yargs");
-const chalk = require("chalk");
 const gitnore = require("./gitnore");
-
-const log = console.log;
 
 const argv = yargs
   .help()
@@ -11,12 +8,4 @@ const argv = yargs
   .epilog("For more information visit https://github.com/mathiasdonoso/gitnore")
   .showHelpOnFail(false, "whoops, something went wrong! run with --help").argv;
 
-if (argv._[0]) {
-  gitnore.createFileForParam();
-
-  argv._.forEach((arg) => {
-    gitnore.addToGitignore(arg.toLowerCase());
-  });
-
-  log(chalk.green(".gitignore file created."));
-}
+gitnore.init(argv._);
