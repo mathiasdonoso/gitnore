be49c63600a10b2b8c998cf2bc243a951efb7d57
Mathías <mathiasd88@gmail.com>
2023-06-10 17:39:21 -0400

    fix extra file generated

--- src/gitnore.js
+++ src/gitnore.js
@@ -1,20 +1,45 @@
 #! /usr/bin/env node
-const fs = require("fs");
-const path = require("path");
-const chalk = require("chalk");
-const filesFolder = path.join(__dirname, "../assets/gitignorefiles/");
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const filesFolder = path.join(__dirname, '../assets/gitignorefiles/');
 
 const log = console.log;
 
-const tempGitignoreFileName = `${new Date().getTime()}.gitignore`;
+const gitignoreFileName = `.gitignore`;
 
-const addToGitignore = (filename) => {
+const init = (args) => {
+  if (args.length > 0) {
+    const writeStream = fs.createWriteStream(gitignoreFileName);
+
+    try {
+      args.forEach((file) => {
+        writeStream.write(`\n${getContentFrom(file)}`);
+      });
+
+      log(chalk.green('.gitignore file created successfully'));
+    } catch (error) {
+      clear();
+      log(chalk.red(error));
+    } finally {
+      writeStream.end();
+    }
+  } else {
+    log(
+      chalk.yellow(
+        'You need to specify which gitignore file want to be created. checkout the available options at https://github.com/mathiasdonoso/gitnore#readme'
+      )
+    );
+  }
+};
+
+const getContentFrom = (filename) => {
   filename = filename.toLowerCase();
 
   try {
     const data = fs.readFileSync(`${filesFolder}${filename}.gitignore`);
 
-    fs.appendFileSync(tempGitignoreFileName, `\n${data}`);
+    return data;
   } catch (error) {
     throw new Error(
       `There is no gitignore file available for "${filename}", checkout the available options at https://github.com/mathiasdonoso/gitnore#readme`
@@ -22,43 +47,8 @@ const addToGitignore = (filename) => {
   }
 };
 
-const createGitignoreFile = () => {
-  fs.createWriteStream(tempGitignoreFileName);
-};
-
-const writeGitignoreFile = (args) => {
-  args.forEach((file) => {
-    addToGitignore(file);
-  });
-};
-
-const replaceGitignoreTempFile = () => {
-  fs.renameSync(tempGitignoreFileName, ".gitignore");
-};
-
 const clear = () => {
-  fs.unlinkSync(tempGitignoreFileName);
-};
-
-const init = (args) => {
-  if (args.length > 0) {
-    try {
-      createGitignoreFile();
-      writeGitignoreFile(args);
-      replaceGitignoreTempFile();
-
-      log(chalk.green(".gitignore file created successfully"));
-    } catch (error) {
-      clear();
-      log(chalk.red(error));
-    }
-  } else {
-    log(
-      chalk.yellow(
-        "You need to specify which gitignore file want to be created. checkout the available options at https://github.com/mathiasdonoso/gitnore#readme"
-      )
-    );
-  }
+  fs.unlinkSync(gitignoreFileName);
 };
 
 module.exports = {
