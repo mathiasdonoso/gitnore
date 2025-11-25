9912dfcc7dcf0d95f71dec8322b2fa566a3704c3
Mathias Donoso <mathiasd88@gmail.com>
2025-11-24 23:13:32 -0300

    feat: Reading files from git module instead of http request (#31)

--- src/gitnore.js
+++ src/gitnore.js
@@ -1,16 +1,18 @@
 #! /usr/bin/env node
 
-import { createWriteStream } from 'fs';
+import { readFileSync, writeFile } from 'fs';
+import { readdirSync } from 'node:fs';
 
-const GIT_IGNORE_FILE_NAME = `.gitignore`;
+const GITIGNORE_FILENAME = `.gitignore`;
 const DOC_TEXT = 'Checkout the available options at https://github.com/mathiasdonoso/gitnore#readme';
+const GITIGNORE_FOLDER_PATH = './vendor/gitignore/';
 
 /**
   * @param {string[]} args
   */
 async function init(args) {
   if (args.length === 0) {
-    console.warn(`You need to specify which gitignore file want to be created. ${DOC_TEXT}`)
+    console.warn(`You need to specify which gitignore file want to be created. ${DOC_TEXT}`);
     return;
   }
 
@@ -20,44 +22,25 @@ async function init(args) {
 
     if (text !== '') {
       content += `${text}\n`;
-      console.info(`Obtained .gitignore info for ${file}.`)
+      console.info(`Obtained .gitignore info for ${file}.`);
     } else {
-      console.warn(`Couldn't found .gitignore info for ${file}. The filename must match the one located in https://github.com/github/gitignore`)
+      console.warn(`Couldn't found .gitignore info for ${file}. The filename must match the one located in https://github.com/github/gitignore`);
     }
   }
 
   if (content === '') {
-    console.warn(`Couldn't create an empty .gitignore file.`)
-    return
+    console.warn(`Couldn't create an empty .gitignore file.`);
+    return;
   }
 
-  const writeStream = createWriteStream(GIT_IGNORE_FILE_NAME, { encoding: 'utf8' });
-  writeStream.write(`${content}`, function(err) {
+  writeFile(GITIGNORE_FILENAME, content, err => {
     if (err) {
       console.error(err);
     } else {
-      console.info(`Added .gitignore file.`)
-      writeStream.end();
+      console.info(`Added .gitignore file.`);
     }
   });
 }
-
-const URLS = [
-  'https://raw.githubusercontent.com/github/gitignore/main',
-  'https://raw.githubusercontent.com/github/gitignore/main/Global',
-  'https://raw.githubusercontent.com/github/gitignore/main/community',
-  'https://raw.githubusercontent.com/github/gitignore/main/community/AWS',
-  'https://raw.githubusercontent.com/github/gitignore/main/community/Dotnet',
-  'https://raw.githubusercontent.com/github/gitignore/main/community/Elixir',
-  'https://raw.githubusercontent.com/github/gitignore/main/community/GNOME',
-  'https://raw.githubusercontent.com/github/gitignore/main/community/Golang',
-  'https://raw.githubusercontent.com/github/gitignore/main/community/Java',
-  'https://raw.githubusercontent.com/github/gitignore/main/community/Javascript',
-  'https://raw.githubusercontent.com/github/gitignore/main/community/Linux',
-  'https://raw.githubusercontent.com/github/gitignore/main/community/PHP',
-  'https://raw.githubusercontent.com/github/gitignore/main/community/Python',
-  'https://raw.githubusercontent.com/github/gitignore/main/community/embedded',
-];
 
 /**
   * @param {string} filename
@@ -65,18 +48,24 @@ const URLS = [
   */
 async function getContentFor(filename) {
   try {
-    for (let baseUrl of URLS) {
-      const endpoint = `${baseUrl}/${filename}.gitignore`;
-      const response = await fetch(endpoint);
-      if (response.status !== 200) {
-        continue;
+    /**
+     * @type {string[]}
+    */
+    const files = readdirSync(GITIGNORE_FOLDER_PATH, { recursive: true }).filter(file => file.includes('.gitignore'));
+
+    const file = files.find(f => {
+      const lastSlash = f.lastIndexOf('/');
+      let fileRenamed = f;
+      if (lastSlash > -1) {
+        fileRenamed = f.substring(lastSlash + 1, f.length);
       }
 
-      const text = await response.text();
-      return text;
-    }
+      return fileRenamed.toLowerCase() === `${filename.toLowerCase()}.gitignore`;
+    });
 
-    return '';
+    if (!file) return '';
+
+    return readFileSync(`${GITIGNORE_FOLDER_PATH}/${file}`, 'utf8');
   } catch (err) {
     throw new Error(err);
   }
